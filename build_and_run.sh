1e49b403664412378ac9c6f72a33bc8aa44d994d
Roland Rodde <rodde@numbercruncher.local>
2020-03-23 20:47:24 +0100

    Splitted build_and_run script into two

--- build_and_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker build -t tensorflow_gpu .
-docker run --gpus all -d -p 0.0.0.0:8888:8888 -p 0.0.0.0:6006:6006 -v /data/Coursera/Advanced_Machine_Learning:/root --name coursera-tensorflow-gpu tensorflow_gpu

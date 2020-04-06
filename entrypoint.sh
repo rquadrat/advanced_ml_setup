e3a5d7ba8a29af9cd41d4cee9bb002effa36d78c
Roland Rodde <rodde@numbercruncher.local>
2020-04-06 23:24:38 +0200

    Tensorboard not woorking any longer without ldconfig. Added scikit-image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+ldconfig
 # jupyter notebook --ip=0.0.0.0 --no-browser --allow-root &
 xvfb-run -s "-screen 0 1400x900x24" jupyter notebook --ip=0.0.0.0 --no-browser --allow-root &
 tensorboard --logdir /root/logs
